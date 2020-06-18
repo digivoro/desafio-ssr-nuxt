0a3cbc3f68f6eaba848b978eecc9fa6a2e782e02
Felipe Castro Richter <nkardia@gmail.com>
2020-06-18 15:43:36 -0400

    Initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-app>
+    <v-app-bar :clipped-left="clipped" fixed app>
+      <v-toolbar-title v-text="'Desafio SSR con Nuxt'" />
+      <v-spacer />
+    </v-app-bar>
+    <v-content>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-content>
+    <v-footer :fixed="fixed" app>
+      <span>Felipe Castro Richter &copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      clipped: false,
+      drawer: false,
+      fixed: false,
+      items: [
+        {
+          icon: "mdi-apps",
+          title: "Welcome",
+          to: "/"
+        },
+        {
+          icon: "mdi-chart-bubble",
+          title: "Inspire",
+          to: "/inspire"
+        }
+      ],
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: "Vuetify.js"
+    };
+  }
+};
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-layout column justify-center align-center>
+    <v-container>
+      <v-row>
+        <v-col v-for="(personaje, index) in personajes" :key="index" cols="4">
+          <v-card>
+            <v-img class="white--text align-end" :src="personaje.imagen">
+            </v-img>
+            <v-card-title class="headline">
+              {{ personaje.nombre }}
+            </v-card-title>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-layout>
+</template>
+
+<script>
+export default {
+  components: {},
+  computed: {
+    personajes() {
+      return this.$store.state.personajes.personajes;
+    }
+  }
+};
+</script>
